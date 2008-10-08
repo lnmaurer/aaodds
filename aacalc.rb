ff6a2f255a96e621eaee88ba88de3112a54d4166
Leon Maurer <Leon.N.Maurer.08@alum.dartmouth.org>
2008-10-07 22:56:27 -0400

    modified license

--- aacalc.rb
+++ aacalc.rb
@@ -2,20 +2,21 @@
 
 # aacalc -- An odds calculator for Axis and Allies
 #Copyright (C) 2008  Leon N. Maurer
-# 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#version 2 as published by the Free Software Foundation;
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#A copy of the license is available at 
+#<http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+#You can also receive a paper copy by writing the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 
 require 'tkextlib/tile'
 require 'yaml'

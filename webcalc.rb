854683d5fb556157c28e1667326e5bc5767acb19
Leon N. Maurer <leon.n.maurer.08@alum.dartmouth.org>
2009-08-25 14:14:37 -0500

    now pages are valid XHTML 1.0 Strict

--- webcalc.rb
+++ webcalc.rb
@@ -695,85 +695,120 @@ end
 __END__
 
 @@ index
-%h1='aacalc'
-%p
-  Instructions: enter information, click 'Calculate', and wait.
-%form{:method => 'POST', :action => "/result"}
-  %table
-    %tr
-      %td{:colspan=>"2"}
-        %h2='Tech'
-        %p
-          %input{:type =>'checkbox', :name=>'tech', :value=>'AAGun'} AA Gun
-          %input{:type =>'checkbox', :name=>'tech', :value=>'HeavyBombers'} Heavy Bombers
-          %input{:type =>'checkbox', :name=>'tech', :value=>'CombinedBombardment'} Combined Bombardment
-          %input{:type =>'checkbox', :name=>'tech', :value=>'Jets'} Jets
-          %input{:type =>'checkbox', :name=>'tech', :value=>'SuperSubs'} Super Subs
-    %tr
-      %td
-        %h2='Attackers'
-        %p
-          %input{:type =>'text', :size => '3', :name=>'aInfantry'} Infantry
-          %br
-          %input{:type =>'text', :size => '3', :name=>'aTank'} Tanks
-          %br
-          %input{:type =>'text', :size => '3', :name=>'aArtillery'} Artillery
-          %br
-          %input{:type =>'text', :size => '3', :name=>'aFighter'} Fighter
-          %br
-          %input{:type =>'text', :size => '3', :name=>'aBomber'} Bomber
-          %br
-          %input{:type =>'text', :size => '3', :name=>'aDestroyer'} Destroyer
-          %br
-          %input{:type =>'text', :size => '3', :name=>'aBattleship'} Battleship
-          %br
-          %input{:type =>'text', :size => '3', :name=>'aTransport'} Transport
-          %br
-          %input{:type =>'text', :size => '3', :name=>'aSubmarine'} Submarine
-      %td
-        %h2='Defenders'
-        %p
-          %input{:type =>'text', :size => '3', :name=>'dInfantry'} Infantry
-          %br
-          %input{:type =>'text', :size => '3', :name=>'dTank'} Tanks
-          %br
-          %input{:type =>'text', :size => '3', :name=>'dArtillery'} Artillery
-          %br
-          %input{:type =>'text', :size => '3', :name=>'dFighter'} Fighter
-          %br
-          %input{:type =>'text', :size => '3', :name=>'dBomber'} Bomber
-          %br
-          %input{:type =>'text', :size => '3', :name=>'dDestroyer'} Destroyer
-          %br
-          %input{:type =>'text', :size => '3', :name=>'dBattleship'} Battleship
-          %br
-          %input{:type =>'text', :size => '3', :name=>'dTransport'} Transport
-          %br
-          %input{:type =>'text', :size => '3', :name=>'dSubmarine'} Submarine
-    %tf
-      %td{:colspan=>"2"}
-        %input{:type => :submit, :value => "Calculate"}
+!!! Strict
+%html{:xmlns => "http://www.w3.org/1999/xhtml", "xml:lang" => "en", :lang => "en"}
+  %head
+    %meta{"http-equiv" => "Content-type", :content =>" text/html;charset=UTF-8"}
+    %title aacalc
+  %body
+    %h1='aacalc'
+    %p
+      Instructions: enter information, click 'Calculate', and wait.
+    %form{:method => 'post', :action => "/result"}
+      %table
+        %tr
+          %td{:colspan=>"2"}
+            %h2='Tech'
+            %p
+              AA Gun
+              %input{:type =>'checkbox', :name=>'tech', :value=>'AAGun'}
+              Heavy Bombers
+              %input{:type =>'checkbox', :name=>'tech', :value=>'HeavyBombers'}
+              Combined Bombardment
+              %input{:type =>'checkbox', :name=>'tech', :value=>'CombinedBombardment'}
+              Jets
+              %input{:type =>'checkbox', :name=>'tech', :value=>'Jets'}
+              Super Subs
+              %input{:type =>'checkbox', :name=>'tech', :value=>'SuperSubs'}
+        %tr
+          %td
+            %h2='Attackers'
+            %p
+              %input{:type =>'text', :size => '3', :name=>'aInfantry'}
+              Infantry
+              %br
+              %input{:type =>'text', :size => '3', :name=>'aTank'}
+              Tanks
+              %br
+              %input{:type =>'text', :size => '3', :name=>'aArtillery'}
+              Artillery
+              %br
+              %input{:type =>'text', :size => '3', :name=>'aFighter'}
+              Fighter
+              %br
+              %input{:type =>'text', :size => '3', :name=>'aBomber'}
+              Bomber
+              %br
+              %input{:type =>'text', :size => '3', :name=>'aDestroyer'}
+              Destroyer
+              %br
+              %input{:type =>'text', :size => '3', :name=>'aBattleship'}
+              Battleship
+              %br
+              %input{:type =>'text', :size => '3', :name=>'aTransport'}
+              Transport
+              %br
+              %input{:type =>'text', :size => '3', :name=>'aSubmarine'}
+              Submarine
+          %td
+            %h2='Defenders'
+            %p
+              %input{:type =>'text', :size => '3', :name=>'dInfantry'}
+              Infantry
+              %br
+              %input{:type =>'text', :size => '3', :name=>'dTank'}
+              Tanks
+              %br
+              %input{:type =>'text', :size => '3', :name=>'dArtillery'}
+              Artillery
+              %br
+              %input{:type =>'text', :size => '3', :name=>'dFighter'}
+              Fighter
+              %br
+              %input{:type =>'text', :size => '3', :name=>'dBomber'}
+              Bomber
+              %br
+              %input{:type =>'text', :size => '3', :name=>'dDestroyer'}
+              Destroyer
+              %br
+              %input{:type =>'text', :size => '3', :name=>'dBattleship'}
+              Battleship
+              %br
+              %input{:type =>'text', :size => '3', :name=>'dTransport'}
+              Transport
+              %br
+              %input{:type =>'text', :size => '3', :name=>'dSubmarine'}
+              Submarine
+        %tr
+          %td{:colspan=>"2"}
+            %input{:type => :submit, :value => "Calculate"}
 
 @@ results
-%h1='Results'
-%h2='Summary of odds'
-%p
-  Attacker wins: #{$battle_details['Summary of odds']['Attacker wins']}
-  %br
-  Defender wins: #{$battle_details['Summary of odds']['Defender wins']}
-  %br
-  Mutual annihilation: #{$battle_details['Summary of odds']['Mutual annihilation']}
-%h2='Tech'
-- if $battle_details['Bomardment']
-  %h2='Bombarders'
-  %ul
-    - $battle_details['Bombarders'].each do |s|
-      %li=s
-%h2='Attacking units and odds'
-%ul
-  - $battle_details['Attacking units and odds'].each do |s|
-    %li=s
-%h2='Defending units and odds'
-%ul
-  - $battle_details['Defending units and odds'].each do |s|
-    %li=s
+!!! Strict
+%html{:xmlns => "http://www.w3.org/1999/xhtml", "xml:lang" => "en", :lang => "en"}
+  %head
+    %meta{"http-equiv" => "Content-type", :content =>" text/html;charset=UTF-8"}
+    %title Battle Results
+  %body
+    %h1='Results'
+    %h2='Summary of odds'
+    %p
+      Attacker wins: #{$battle_details['Summary of odds']['Attacker wins']}
+      %br
+      Defender wins: #{$battle_details['Summary of odds']['Defender wins']}
+      %br
+      Mutual annihilation: #{$battle_details['Summary of odds']['Mutual annihilation']}
+    %h2='Tech'
+    - if $battle_details['Bomardment']
+      %h2='Bombarders'
+      %ul
+        - $battle_details['Bombarders'].each do |s|
+          %li=s
+    %h2='Attacking units and odds'
+    %ul
+      - $battle_details['Attacking units and odds'].each do |s|
+        %li=s
+    %h2='Defending units and odds'
+    %ul
+      - $battle_details['Defending units and odds'].each do |s|
+        %li=s
